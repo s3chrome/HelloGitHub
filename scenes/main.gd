d81dbc39a3ff0d4054858da5fdde92c6ea5f34bf
s3chrome <90816366+s3chrome@users.noreply.github.com>
2024-05-08 02:40:07 +0900

    add player

---
+++ scenes/main.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const SPEED = 300.0
+
+@onready var player := $Sprite2D
+@onready var label := $Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if dir:
+		player.position += dir * SPEED * delta
+		label.text = "%s" % player.position
