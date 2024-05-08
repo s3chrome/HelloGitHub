b5c764da45444194443eb835ba905da4181b2087
s3chrome <90816366+s3chrome@users.noreply.github.com>
2024-05-08 13:06:35 +0900

    add exec button

--- scenes/main.gd
+++ scenes/main.gd
@@ -7,6 +7,7 @@ const SPEED = 300.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Button.pressed.connect(my_func)
 	pass # Replace with function body.
 
 
@@ -19,3 +20,26 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("A"):
 		label.text = "press a"
 		
+func my_func():
+	#
+	if OS.has_feature('web'):
+		JavaScriptBridge.eval("""
+			console.log('The JavaScriptBridge singleton is available')
+		""")
+	else:
+		print("The JavaScriptBridge singleton is NOT available")
+
+	#
+	JavaScriptBridge.eval("alert('Calling JavaScript per GDScript!');")
+
+	# number to float
+	# boolean to bool
+	# string to String
+	# ArrayBuffer,TypedArray,DataView to PackedByteArray
+	# other to null
+	var js_return = JavaScriptBridge.eval("var myNumber = 1; myNumber + 2;")
+	print(js_return) # prints '3.0'
+	
+	# execute in global execution context,
+	# thus adding a new JavaScript global variable `SomeGlobal`
+	JavaScriptBridge.eval("var SomeGlobal = {};", true)
